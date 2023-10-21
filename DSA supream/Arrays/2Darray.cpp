41a645c3077ef545b7427567a77256687c455114
TEJ <123865368+tej777K@users.noreply.github.com>
2023-10-21 20:35:29 +0530

    pushed

---
+++ DSA supream/Arrays/2Darray.cpp
@@ -0,0 +1,72 @@
+#include<iostream>
+using namespace std;
+
+
+// void printArray(int arr[][4] , int row , int col ){
+
+
+//printed in row wise 
+// for(int i=0 ; i<row ; i++){
+//     for(int j=0 ; j<col  ; j++){
+//         cout<<arr[i][j]<<" ";
+//     }
+//     cout<<endl;
+
+// }
+
+//printed in coloumn wise 
+
+// for(int i=0 ; i<col ; i++){
+//     for(int j=0 ; j<row  ; j++){
+//         cout<<arr[j][i]<<" ";
+//     }
+//     cout<<endl;
+
+// }
+
+//}
+
+// bool findtarget(int arr[][4],int row , int col , int target){
+//     for(int i=0 ; i<row ; i++){
+//     for(int j=0 ; j<col  ; j++){
+//        if(arr[i][j]==target){
+//         cout<<"At : "<<i<<" "<<j<<endl;
+//         cout<<arr[i][j]<<" ";
+//         return true;
+//        }
+//     }
+//     cout<<endl;
+
+// }
+
+// }
+
+
+// int main(){
+
+
+// //minumum we have to give the size of the array
+// // int arr[][4] ={
+   
+// //    {1,2,3,4},
+// //    {5,6,7,8},
+// //    {3,4,5,6}
+
+// // };
+
+
+// int arr[3][4] ={
+   
+//    {1,2,3,4},
+//    {5,6,7,8},
+//    {3,4,5,6}
+
+// };
+// int row = 3;
+// int col = 4;
+// int target = 4;
+// // printArray(arr,row,col);
+// cout<<"Fount or not : "<<findtarget(arr,row,col,target);
+
+//     return 0;
+// }
--- DSA supream/Arrays/vector.cpp
+++ DSA supream/Arrays/vector.cpp
@@ -4,38 +4,126 @@
 #include<vector>
 using namespace std;
 
-void print(vector<int>v){
-    int size = v.size();
-    cout<<"the elements of vector array are : ";
-    for(int i =0 ; i<size  ; i++){
-        cout<<v[i]<<" ";
-    }
+// void print(vector<int>v){
+//     int size = v.size();
+//     cout<<"the elements of vector array are : ";
+//     for(int i =0 ; i<size  ; i++){
+//         cout<<v[i]<<" ";
+//     }
 
    
-}
+// }
+
+// int main(){
+//     //in vector don't tell the size 
+//     //just keep inserting , it will amnage the allocations
+//     //vectors are noting but the arrays only!
+
+//     vector<int>v;
+
+//     //insert 
+//     // v.push_back(1);
+//     // v.push_back(2);
+//     // v.push_back(3);
+
+//     while (1)
+//     {
+//         int d;cin>>d;
+//         v.push_back(d);
+//         cout<<"Capcity : "<<v.capacity()<<" "<<"Size : "<<v.size()<<endl;
+//     }
+    
+
+//     print(v);
+    
+//     return 0;
+
+// }
+
+
+// int main(){
+
+//     vector<int> arr;
+     
+//      for(int i=0;i<=5;i++)
+//      arr.push_back(i);
+
+//      cout<<"Size : "<<arr.size()<<endl;
+//      cout<<"Capacity : "<<arr.capacity()<<endl;
+//      cout<<"max_Size: "<<arr.max_size()<<endl;
+
+//      for(int i =0 ; i<arr.size()  ; i++){
+//         cout<<arr[i]<<" ";
+//     }
+//     cout<<endl;
+
+//      arr.resize(4);
+
+//      cout<<"Size : "<<arr.size()<<endl;
+
+//    for(auto k=arr.begin();k!=arr.end();k++){
+//     cout<<*k<<" ";
+//    }
+
+// vector<int>v;
+
+// for(int i =1 ;i<=5 ; i++){
+//     v.push_back(i);
+
+// }
+// cout<<"reference operator : [v]: "<<v[4]<<endl;
+// cout<<"v.at: "<<v.at(4)<<endl;
+// cout<<"front: "<<v.front()<<endl;
+// cout<<"back: "<<v.back()<<endl;
+
+
+//     return  0;
+
+// }
+
+
+//2D Vector 
 
 int main(){
-    //in vector don't tell the size 
-    //just keep inserting , it will amnage the allocations
-    //vectors are noting but the arrays only!
+    
+    // vector<int>v;
 
-    vector<int>v;
+    // cout<<v.size()<<endl;
+    // cout<<v.capacity();
 
-    //insert 
-    // v.push_back(1);
-    // v.push_back(2);
-    // v.push_back(3);
+//  vector<vector<int> >v(5,vector<int>(10,7));
 
-    while (1)
-    {
-        int d;cin>>d;
-        v.push_back(d);
-        cout<<"Capcity : "<<v.capacity()<<" "<<"Size : "<<v.size()<<endl;
+//  for(int i=0 ; i<v.size();i++){
+//     for(int j=0 ; j<v[i].size();j++){
+//         cout<<v[i][j]<<" ";
+//     }
+//     cout<<endl;
+//  }
+
+
+vector<vector<int> >v;
+
+vector<int>v1(10,0);
+vector<int>v2(5,1);
+vector<int>v3(7,0);
+vector<int>v4(20,1);
+vector<int>v5(3,0);
+
+v.push_back(v1);
+v.push_back(v2);
+v.push_back(v3);
+v.push_back(v4);
+v.push_back(v5);
+
+
+ for(int i=0 ; i<v.size();i++){
+    for(int j=0 ; j<v[i].size();j++){
+        cout<<v[i][j]<<" ";
     }
-    
+    cout<<endl;
+ }
 
-    print(v);
-    
-    return 0;
 
+
+  
 }
