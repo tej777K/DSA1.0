3ec3b7ee8164b266695c8f22974fad6faabc988c
TEJ <123865368+tej777K@users.noreply.github.com>
2023-12-20 22:41:02 +0530

    Updated

--- DSA supream/CharArray&Strings/problems/Leetcode680.cpp
+++ DSA supream/CharArray&Strings/problems/Leetcode680.cpp
@@ -53,3 +53,38 @@ int main(){
 
     return 0;
 }
+
+
+
+// class Solution {
+// public:
+    
+//     bool checkPallindrome(string s , int i , int j){
+//         while(i<=j){
+//             if(s[i] != s[j]){
+//                 return false;
+//             }
+//         else{
+//             i++;
+//             j--;
+//         }
+        
+//         }
+//         return true;
+//     }
+    
+
+//     bool validPalindrome(string s) {
+//        int i  =0 ;
+//        int  j = s.length() - 1;
+//        while(i<=j){
+//            if(s[i] != s[j]){
+//                return checkPallindrome(s,i+1,j) || checkPallindrome(s,i,j-1);
+//            }else{
+//                i++;
+//                j--;
+//            }
+//        } 
+//        return true;
+//     }
+// };
